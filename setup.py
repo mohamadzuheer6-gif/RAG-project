34c4a8a4af43b011fe5fb28b217aed7573a55729
Mohammad Zuheer <mohamadzuheer6@gmial.com>
2026-02-17 23:56:42 +0530

    requirements pushed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Mohammad Zuheer",
+    author_email="mohamadzuheer6@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
