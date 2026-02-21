8eb732ab99565cdc0391571d90ab74cfeb29d978
Mohammad Zuheer <mohamadzuheer6@gmial.com>
2026-02-21 17:46:52 +0530

    added gunicorn and port config

--- app.py
+++ app.py
@@ -70,3 +70,9 @@ if __name__ == "__main__":
         db.create_all()
 
     app.run(debug=True)
+    
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
